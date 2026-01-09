8afbd252e10caf947c11a6ee79c4a334dbd9b83f
prajapativipul4395-hub <prajapativipul4395@gmail.com>
2026-01-09 14:49:03 +0530

    08

--- currencyConvertor/src/App.jsx
+++ currencyConvertor/src/App.jsx
@@ -12,7 +12,7 @@ function App() {
 
   const currencyInfo = useCurrencyInfo(from)
 
-  const options = Object.keys(currencyInfo || {}) 
+  const options = Object.keys(currencyInfo || {})
 
   const swap = () => {
     setFrom(to)
@@ -32,14 +32,17 @@ function App() {
 
 
   return (
+     
     <div
       className="w-full h-screen flex flex-wrap justify-center items-center bg-cover bg-no-repeat"
       style={{
         backgroundImage: `url('https://images.pexels.com/photos/3532540/pexels-photo-3532540.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2')`,
       }}
     >
+      
       <div className="w-full">
         <div className="w-full max-w-md mx-auto border border-gray-60 rounded-lg p-5 backdrop-blur-sm bg-white/30">
+          
           <form
             onSubmit={(e) => {
               e.preventDefault();
@@ -57,6 +60,7 @@ function App() {
                 onAmountChange={(amount) => setAmount(amount)}
               />
             </div>
+            
             <div className="relative w-full h-0.5">
               <button
                 type="button"
@@ -73,7 +77,7 @@ function App() {
                 currencyOptions={options}
                 onCurrencyChange={(currency) => setTo(currency)}
                 selectCurrency={to}
-                amountDisable
+                amountDisable 
               />
             </div>
             <button type="submit" className="w-full bg-blue-600 text-white px-4 py-3 rounded-lg">
