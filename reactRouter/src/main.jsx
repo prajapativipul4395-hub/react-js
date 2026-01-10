ea1a4d299f0b3439562040d4322d89d1afecde70
prajapativipul4395-hub <prajapativipul4395@gmail.com>
2026-01-11 02:52:35 +0530

    ..

--- reactRouter/src/main.jsx
+++ reactRouter/src/main.jsx
@@ -8,6 +8,7 @@ import Home from './components/Home/Home.jsx'
 import About   from './components/About/About.jsx'
 import Contact from './components/Contact/Contact.jsx'
 import User from './components/User/User.jsx'
+import Github, { githubInfoLoader } from './components/Github/Github.jsx'
 
 // const router = createBrowserRouter([
 //   {
@@ -36,9 +37,14 @@ const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout />} >
       <Route path='' element={<Home />} />
-      <Route path='About' element={<About />} />
-      <Route path='Contact' element={<Contact />} />
-      <Route path='User/:userId' element={<User />} />
+      <Route path='about' element={<About />} />
+      <Route path='contact' element={<Contact />} />
+      <Route path='user/:userId' element={<User />} />
+      <Route 
+      loader={githubInfoLoader}
+      path='github' 
+      element={<Github />} 
+      />
     </Route>
 
   )
